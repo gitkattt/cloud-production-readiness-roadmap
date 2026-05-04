98eaa4d6f1a2265123a19f88a92e8dd38d47669c
Jon Davis <rayyedits76d@gmail.com>
2026-05-04 18:58:23 -0400

    Update provider.tf

--- Week 10 – Terraform Remote State Backend/Code/provider.tf
+++ Week 10 – Terraform Remote State Backend/Code/provider.tf
@@ -1,6 +1,6 @@
 terraform {
    backend s3 {
-        bucket = "s3-state-back-end-667"
+        bucket = "bucket-name-here"
         dynamodb_table = "state-lock"
        key = "global/mystatefile/terraform.tfstate"
         region = "us-east-1"
